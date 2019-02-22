5eed87cc51bff8d5f8c954098cc406c0b74aa240
Ruben van Drongelen <ruben.van.drongelen@gmail.com>
2019-02-22 18:15:30 +0100

    first "working" example

---
+++ SubDialogBot/ChildDialog.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SubDialogBot
+{
+    public class ChildDialog : ComponentDialog
+    {
+        private const string childName = "childName";
+        private const string childWaterfallName = "childWaterfallName";
+        public ChildDialog() : base(nameof(ChildDialog))
+        {
+            var waterfallSteps = new WaterfallStep[]
+            {
+                SayHello
+            };
+            AddDialog(new WaterfallDialog(childWaterfallName, waterfallSteps));
+            AddDialog(new TextPrompt(childName));
+        }
+
+        private async Task<DialogTurnResult> SayHello(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("Hello from child."));
+            return await stepContext.EndDialogAsync();
+        }
+    }
+}
---
+++ SubDialogBot/ParentDialog.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SubDialogBot
+{
+    public class ParentDialog : ComponentDialog
+    {
+        private const string waterfallDialogName = "parentName";
+
+        public ParentDialog() : base(nameof(ParentDialog))
+        {
+            AddDialog(new ChildDialog());
+
+            var waterfallSteps = new WaterfallStep[]
+            {
+                SayHello,
+                SayHello2
+            };
+            AddDialog(new WaterfallDialog(waterfallDialogName, waterfallSteps));
+        }
+
+        private async Task<DialogTurnResult> SayHello(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("Hello from parent."));
+            return await stepContext.NextAsync();
+        }
+
+        private async Task<DialogTurnResult> SayHello2(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text("Hello from parent2."));
+            return await stepContext.NextAsync();
+        }
+    }
+}
